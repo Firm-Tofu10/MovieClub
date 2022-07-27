6abdb0ca3353a851c132a667b9240a2c7c072b82
Bryson.Tofu <70546630+Firm-Tofu10@users.noreply.github.com>
2022-07-26 20:50:10 -0700

    SQL
    Querys,Schema,Seeds updated

--- DB/schema.sql
+++ DB/schema.sql
@@ -2,24 +2,4 @@ DROP DATABASE IF EXISTS mData_db;
 CREATE DATABASE mData_db;
 
 USE mData_db;
-
-CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  department_name VARCHAR(100) NOT NULL
-);
-
-CREATE TABLE roles (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL(10,2),
-    department_id INT references department(id)
-  
-);
-
-CREATE TABLE employees (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT references roles(id),
-    manager_id INT references employees(id) on delete set NULL);
     
--- DB/seeds.sql
+++ DB/seeds.sql
@@ -1,8 +1,8 @@
 USE Mdata_db;
 
-insert into titles(titles_name) values('Marketing'),('Sales'),('IT');
+insert into titles(titles_name) values("Alien"),("Raiders of the Lost Ark");
 
-INSERT into ratings(G,PG,PG13,R)values('Manager',248734,1),('Manager',218764,2),('Manager',214,3);
+INSERT into ratings(G,PG,PG13,R)values("G"),("PG"),("PG13"),("R");
 
-INSERT into stars(one,two,three,four,five)values('Bryson','Benson',1),('Charlie','Gillbers',2),('Ben','Garnet',3);
+INSERT into stars(one,two,three,four,five)values("One",1),("Two",2),("Three",3),("Four",4),("Five",5);
 
