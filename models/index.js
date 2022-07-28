72ab6ceed0b858172316fa2f14c58f98246771af
kate patch <kate.epatch@gmail.com>
2022-07-28 15:19:51 -0600

    made comments for all the model exports we need

--- models/index.js
+++ models/index.js
@@ -1,2 +1,22 @@
 const User = require('./User');
 const Review = require('./Review');
+const Reviewer = require('./Reviewer');
+const Comment = require('./Comment');
+
+//Reviewer has many reviews
+
+
+
+
+//Review can only belong to Reviewer
+
+
+
+//User has many comments
+
+
+
+//Comment can only belong to one User
+
+
+//Review can have many comments
