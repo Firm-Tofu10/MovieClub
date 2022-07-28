5bd4e8d939661f8e4a4e0f355f976b9ce9c64a52
kate patch <kate.epatch@gmail.com>
2022-07-28 13:49:40 -0600

    added posts to the routers for users.  gonna see if i can test them in insomnia

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,50 @@
+const router = require('express').Router();
+const { User } = reequire('../../models');
+
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.create(req.body);
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.status(200).json(userData);
+        });
+    } catch (err) {
+        res.status(400).json(err);
+        console.log(err);
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: { email: req.body.email } });
+        
+        if (!userData) {
+            res
+              .status(400)
+              .json({ message: 'Incorrect email or password' });
+            return;
+        }
+
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res
+              .status(400)
+              .json({ message: 'Incorrect email or password' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.json({ user: userData, message: 'Logged in!' });
+        });
+    } catch (err) {
+        res.status(400).json(err);
+        console.log(err);
+    }
+});
