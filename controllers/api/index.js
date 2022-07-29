83103a31a2701cd81fa8067846c864ea6d89819c
kate patch <kate.epatch@gmail.com>
2022-07-28 20:58:22 -0600

    added link to reviewerRoutes to index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const reviewRoutes = require('./reviewRoutes');
 const commentRoutes = require('./commentRoutes');
+const reviewerRoutes = require('./reviewerRoutes');
 
 router.use('/users', userRoutes);
 router.use('/reviews', reviewRoutes);
 router.use('/comments', commentRoutes);
+router.use('/reviewer', reviewerRoutes);
 
 module.exports = router;
