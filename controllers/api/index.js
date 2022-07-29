4cba44f6873d9281a1733300452f64f2904f3594
kate patch <kate.epatch@gmail.com>
2022-07-28 20:53:37 -0600

    added to the index.js for api routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,11 @@
 const router = require('express').Router();
 
-router.use()
+const userRoutes = require('./userRoutes');
+const reviewRoutes = require('./reviewRoutes');
+const commentRoutes = require('./commentRoutes');
+
+router.use('/users' userRoutes);
+router.use('/reviews', reviewRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
