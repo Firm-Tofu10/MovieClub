cd9019378f9ea7031e06399ef6a1c969d5d6b129
Bryson.Tofu <70546630+Firm-Tofu10@users.noreply.github.com>
2022-07-28 21:36:03 -0700

    Server.js wont run
    working on fixing server.js to via node

--- models/index.js
+++ models/index.js
@@ -1,6 +1,5 @@
-const User = require('./User');
-const Review = require('./Review');
-
+// const User = require('./User');
+// const Review = require('./Review');
 // Has many, has one, belongs to relationships?
 
-module.exports = {User, Review};
+// module.exports = {User, Review};
