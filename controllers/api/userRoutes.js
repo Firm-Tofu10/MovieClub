e6aacd53674973691db07f52a72e0ae8c828e24c
kate patch <kate.epatch@gmail.com>
2022-07-28 19:33:39 -0600

    added get for single user

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const { User } = reequire('../../models');
+const { Review } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+//Create new user
 router.post('/', async (req, res) => {
     try {
         const userData = await User.create(req.body);
@@ -18,6 +20,7 @@ router.post('/', async (req, res) => {
     }
 });
 
+//Login for user/verify
 router.post('/login', async (req, res) => {
     try {
         const userData = await User.findOne({ where: { email: req.body.email } });
@@ -50,6 +53,7 @@ router.post('/login', async (req, res) => {
     }
 });
 
+//logout user
 router.post('/logout', (req, res) => {
     if (req.session.logged_in) {
         req.session.destroy(() => {
@@ -59,6 +63,54 @@ router.post('/logout', (req, res) => {
     } else {
         res.status(404).end();
     }
+});
+
+//GET users
+router.get('/', (req, res) => {
+    User.findAll({
+        attributes: { exclude: ['[password]'] }
+    })
+    .then(userData => res.json(userData))
+    .catch(err => {
+        res.status(500).json(err);
+        console.log(err);
+    });
+
+});
+
+//Get single user
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Review,
+                attributes: ['id', 'title', 'description', 'genre', 'reviewer_id']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'user_name', 'review_id'],
+                include: {
+                    model: Review,
+                    attributes: ['title']
+                }
+            }
+        ]
+    })
+    .then(userData => {
+        if(!userData) {
+            res.status(404).json({ message: 'No user found with this id'})
+            return
+        }
+        res.json(userData);
+    })
+    .catch(err => {
+        res.status(500).json(err);
+        console.log(err);
+    });
 });
 
 module.exports = router;
