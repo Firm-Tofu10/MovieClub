607657fe91be0da29f15d82b6c1719d0b56ac51b
kate patch <kate.epatch@gmail.com>
2022-07-28 20:54:14 -0600

    typo

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,7 +4,7 @@ const userRoutes = require('./userRoutes');
 const reviewRoutes = require('./reviewRoutes');
 const commentRoutes = require('./commentRoutes');
 
-router.use('/users' userRoutes);
+router.use('/users', userRoutes);
 router.use('/reviews', reviewRoutes);
 router.use('/comments', commentRoutes);
 
