9d1749b72bf4e0a8393c286823de749f820a6b75
Bryson.Tofu <70546630+Firm-Tofu10@users.noreply.github.com>
2022-07-28 19:20:16 -0700

    Tables Update
    assoadnoiasd

--- DB/Userschema.sql
+++ DB/Userschema.sql
@@ -1,4 +1,12 @@
 DROP DATABASE IF EXISTS Userdata_db;
 CREATE DATABASE Userdata_db;
 
+CREATE TABLE UserLogin (
+  
+  id INT NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  
+  user_name VARCHAR(30) NOT NULL
+);
+
 USE Userdata_db;
--- DB/schema.sql
+++ DB/schema.sql
@@ -1,5 +1,15 @@
 DROP DATABASE IF EXISTS mData_db;
 CREATE DATABASE mData_db;
 
+CREATE TABLE MovieInfo (
+
+    id INT NOT NULL 
+    titles VARCHAR(255) NOT NULL,
+
+    ratings VARCHAR(10) NOT NULL,
+
+    stars INT NOT NULL
+);
+
 USE mData_db;
     
