8efab0d889bb411e15a07817f598e3c353e48ab1
kate patch <kate.epatch@gmail.com>
2022-07-30 12:46:17 -0600

    added route to join reviews with all other data

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,10 +1,35 @@
 const router = require('express').Router();
-const {Review, User} = require('../models')
+const { Review, User, Reviewer, Comment } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
+    const reviewData = await Review.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+        {
+          model: Reviewer,
+          attributer: ['name']
+        },
+        {
+          model: Comment,
+          attributes: ['comment_text']
+        },
+      ],
+    });
+
+    const reviews = reviewData.map((reviews) => reviews.get({ plain: true }));
+
+    res.render('homepage', {
+      reviews,
+      logged_in: req.session.logged_in
+    });
 
   } catch (err) {
-
+    res.status(500).json(err);
+    console.log(err);
   }
 });
