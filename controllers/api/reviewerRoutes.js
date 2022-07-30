843336ef5c9c46055a7fd55e8724e0d30410b7cc
kate patch <kate.epatch@gmail.com>
2022-07-30 09:21:19 -0600

    added login for reviewer

--- controllers/api/reviewerRoutes.js
+++ controllers/api/reviewerRoutes.js
@@ -3,4 +3,34 @@ const { Reviewer, Review } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 //Login Reviewer
+router.post('/login', async (req, res) => {
+    try {
+        const reviewerData = await Reviewer.findOne({ where: { email: req.body.email }});
+
+        if (!reviewerData) {
+            res
+            .status(400)
+            .json({ message: 'Incorrect email or password '});
+            return;
+        }
+
+        const validPassword = await reviewerData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res
+            .status(400)
+            .json({ message: 'Incorrect email or password' });
+            return;
+        }
+        req.session.save(() => {
+            req.session.reviewer_id = reviewerData.isSoftDeleted;
+            req.session.logged_in = true;
+
+            res.json({ reviewer: reviewerData, message: 'Logged in!' });
+        });
+    } catch (err) {
+        res.status(400).json(err);
+        console.log(err);
+    }
+});
 
