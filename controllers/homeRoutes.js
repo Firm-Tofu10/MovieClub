82ebea06e6f66a6da97c4fe4cf4566c6019843cb
Jayden Griffith <griffithjayden97@outlook.com>
2022-08-04 17:51:42 -0600

    Added comments section to reviews.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -21,7 +21,7 @@ router.get('/', async (req, res) => {
 
     res.render('homepage', {
       reviews,
-      logged_in: req.session.logged_in
+      logged_in: req.session.logged_in,
     });
 
   } catch (err) {
