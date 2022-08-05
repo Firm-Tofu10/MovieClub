b49d3e9b0cf1f06c30fd922a6939840a52ae92fd
Bryson.Tofu <70546630+Firm-Tofu10@users.noreply.github.com>
2022-08-04 18:46:56 -0700

    trying to handler the event listener for stars
    trying to handler the event listener for stars

--- dist/js/moviePoll.js
+++ dist/js/moviePoll.js
@@ -36,16 +36,6 @@ for (let i = 0; i < options.length; i++) {
   });
 }
 // to do find parent div with class = fiveToOne
-const starLabels = document.querySelectorAll(".fiveToOne > label");
-for (let i = 0; i < starLabels.length; i++) {
-  starLabels[i].addEventListener("click", (evt)=>{
-    console.log("starLabelsClicked",evt.target.parentNode.querySelectorAll("label"));
-    for (let j = 0; j < starLabels.length; j++) {
-      if(starLabels.contains("selected")){
-        starLabels.remove("selected")
-      }
-}})
-};
 
 // handle lable click here
 // evt selected
